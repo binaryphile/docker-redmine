8d6cb487405bc01a10356bdfa8ee1794b8350ee9
Ted Lilley <tlilley@diditbetter.com>
2013-10-21 15:13:24 -0400

    Update run scripts

--- daemon.sh
+++ daemon.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-ROOT=/root
-IMAGE=binaryphile/redmine:2.3-stable
-DB_ADAPTER=postgresql
-DB_DATABASE=redmine
-DB_HOST=192.168.1.2
-DB_USERNAME=redmine
-DB_PASSWORD=Redmi42.postgresql.
-RAILS_ENV=production
-RM_PORT=3001
-CMD="bundle exec unicorn_rails -c config/unicorn.rb -p $RM_PORT"
-RM_DIR=/redmine
-OPTIONS="-d -w $RM_DIR -v $(pwd):$ROOT -p :$RM_PORT -e RAILS_ENV=$RAILS_ENV -e DB_ADAPTER=$DB_ADAPTER -e DB_DATABASE=$DB_DATABASE -e DB_HOST=$DB_HOST -e DB_USERNAME=$DB_USERNAME -e DB_PASSWORD=$DB_PASSWORD"
-SUDO=""
+: ${IMAGE?"need to set image name IMAGE, see README.md"}
+: ${DB_HOST?"need to set database hostname or ip DB_HOST, see README.md"}
+: ${DB_USERNAME?"need to set database username DB_USERNAME, see README.md"}
+: ${DB_PASSWORD?"need to set database password DB_PASSWORD, see README.md"}
+
+: ${ROOT=/root}
+: ${DB_ADAPTER=postgresql}
+: ${DB_DATABASE=redmine}
+: ${RAILS_ENV=production}
+: ${RM_PORT=3001}
+: ${CMD="bundle exec unicorn_rails -c config/unicorn.rb -p $RM_PORT"}
+: ${RM_DIR=/redmine}
+: ${OPTIONS="-d -w $RM_DIR -v $(pwd):$ROOT -p :$RM_PORT -e RAILS_ENV=$RAILS_ENV -e DB_ADAPTER=$DB_ADAPTER -e DB_DATABASE=$DB_DATABASE -e DB_HOST=$DB_HOST -e DB_USERNAME=$DB_USERNAME -e DB_PASSWORD=$DB_PASSWORD"}
+: ${SUDO=""} # set to "sudo" if you are not in the docker group
 
 $SUDO docker run $OPTIONS $IMAGE $CMD
 
